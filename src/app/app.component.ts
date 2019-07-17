2864d67aacbb14608ee16b7be011eac3a8fd92b5
Eric Neiman <eneiman@amctechnology.com>
2019-07-17 09:19:39 -0400

    Added basic Channel functionality and comments.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,63 +3,127 @@ import {
   initializeComplete,
   InteractionStates,
   setAppHeight,
-  SearchRecords,
-  registerClickToDial
+  registerClickToDial,
+  registerOnPresenceChanged,
+  setPresence
 } from '@amc-technology/davinci-api';
+
+/**
+ * Here the service that will simulate the functionality of a channel API is imported.
+ */
+import { SimulatorService } from './simulator.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   title = 'SampleChannelApp'; // Make sure this title matches the title you gave this app in Creators Studio
-
+  presence = 'Not Ready'; // presence defaults to Not Ready, when Ready you can make or recieve a call
   calls: { id: string; number: string; state?: InteractionStates }[] = [];
-  phoneNumbers = [
-    '555-123-4567',
-    '555-867-5309',
-    '555-000-0001',
-    '555-999-9999'
-  ];
-  nextPhoneNumberIndex = 0;
-  constructor() {
-    this.callSpecific = this.callSpecific.bind(this);
+
+  constructor(private simulatorService: SimulatorService) {
+    // Make sure to bind all functions used as callbacks so that any changes made to variables are in the correct context
+    this.executeClickToDial = this.executeClickToDial.bind(this);
+    this.presenceChange = this.presenceChange.bind(this);
   }
+
   ngOnInit() {
+    // registerClickToDial() lets the DaVinci api know that you are listening for a click to dial event.
+    registerClickToDial(this.executeClickToDial);
+
+    // registerOnPresenceChanged() lets the DaVinci api know that you are listening for a change in presence.
+    registerOnPresenceChanged(this.presenceChange);
+
+    // setAppHeight() controls the height of your app and whether or not your app is visible in the toolbar.
+    // If it is not set your app will not show up in the toolbar.
+    setAppHeight(250);
+
+    // initializeComplete() lets the DaVinci api know that you are done registering for events.
     initializeComplete();
-    registerClickToDial(this.callSpecific);
-    setAppHeight(200);
   }
 
+  /**
+   * This function is used as the callback for the registerClickToDial() api call.
+   *
+   * @param {string} phone
+   * @memberof AppComponent
+   */
+  async executeClickToDial(phone: string) {
+    if (this.presence === 'Ready') {
+      console.log(`Calling ${phone}`);
+      // setPresence() is used to send a new presence to the global presence through the DaVinci api.
+      setPresence('Not Ready', 'Making a Call');
+      console.log('New Call');
+      this.calls = [
+        ...this.calls,
+        this.simulatorService.newOutboundCall(phone)
+      ];
+    } else {
+      console.log(
+        `The presence is ${this.presence}, agent is not accepting calls.`
+      );
+    }
+  }
+
+  /**
+   * This function is used as the callback for the registerOnPresenceChanged() api call.
+   *
+   * @param {string} presence
+   * @param {string} [reason]
+   * @returns promise
+   * @memberof AppComponent
+   */
+  async presenceChange(presence: string, reason?: string) {
+    return new Promise<void>((resolve, reject) => {
+      if (reason === null) {
+        console.log(`Presence Changed: ${presence}`);
+      } else {
+        console.log(`Presence Changed: ${presence} for this reason: ${reason}`);
+      }
+      this.presence = presence;
+    });
+  }
+
+  /**
+   * This function is used to receive a call from the simulator or api.
+   *
+   * @memberof AppComponent
+   */
   newCall() {
-    this.calls = [
-      ...this.calls,
-      {
-        id: `Call-${Math.random()}`,
-        number: this.getNextPhoneNumber()
-      }
-    ];
+    if (this.presence === 'Ready') {
+      // setPresence() is used to send a new presence to the global presence through the DaVinci api.
+      setPresence('Not Ready', 'Receiving a Call');
+      console.log('New Call');
+      this.calls = [...this.calls, this.simulatorService.newCall()];
+    } else {
+      console.log(
+        `The presence is ${this.presence}, agent is not accepting calls.`
+      );
+    }
   }
 
-  private getNextPhoneNumber() {
-    const result = this.phoneNumbers[this.nextPhoneNumberIndex];
-    this.nextPhoneNumberIndex =
-      (this.nextPhoneNumberIndex + 1) % this.phoneNumbers.length;
-    return result;
+  /**
+   * This function increments the index for the array of phone numbers.
+   *
+   * @memberof AppComponent
+   */
+  getNextPhoneNumber() {
+    console.log('Next Phone Number');
+    this.simulatorService.getNextPhoneNumber();
   }
 
-  removeCall(id: string) {
+  /**
+   * This function is called when the call is dropped by any party.
+   *
+   * @param {string} id
+   * @memberof AppComponent
+   */
+  dropCall(id: string) {
+    console.log(`Dropped Call id = ${id}`);
     this.calls = this.calls.filter(call => call.id !== id);
-  }
-  async callSpecific(phone: string) {
-    this.calls = [
-      ...this.calls,
-      {
-        id: `Call-${Math.random()}`,
-        number: phone,
-        state: InteractionStates.Connected
-      }
-    ];
+    // setPresence() is used to send a new presence to the global presence through the DaVinci api.
+    setPresence('Ready', 'Call Dropped');
   }
 }
--- src/app/call/call.component.ts
+++ src/app/call/call.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  OnInit,
-  Input,
-  Output,
-  EventEmitter
-} from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import {
   setInteraction,
   IInteraction,
@@ -16,26 +10,66 @@ import {
   registerScreenpop,
   SearchRecords
 } from '@amc-technology/davinci-api';
+import { timer } from 'rxjs';
+import { clearResolutionOfComponentResourcesQueue } from '@angular/core/src/metadata/resource_loading';
 
+/**
+ * This component handles calls and their interactions.
+ *
+ * @export
+ * @class CallComponent
+ * @implements {OnInit}
+ */
 @Component({
   selector: 'app-call',
   templateUrl: './call.component.html',
-  styleUrls: ['./call.component.css']
+  styleUrls: ['./call.component.scss']
 })
 export class CallComponent implements OnInit {
-  @Input() id: string;
+  @Input() id: number;
   @Input() number: string;
   @Output() callEnd = new EventEmitter();
   interaction: IInteraction;
+  callTimer = timer(0, 1000); // timer for call time that starts when a call is initiated
+  currentTime = 0; // value of the timer in seconds
+  holdTimes = []; //
+  holdStart = 0;
+  holdEnd = 0;
+  totalHoldTime = 0;
+  currentHoldTime = 0;
+  subscription;
 
   constructor() {
+    this.callTimer.subscribe(val => (this.currentTime = val));
   }
 
+  /**
+   * This method formats the timer.
+   *
+   * @param {number} seconds
+   * @returns a string of the format "00m00s"
+   * @memberof CallComponent
+   */
+  formatTimer(seconds: number) {
+    const minutes: number = Math.floor(seconds / 60);
+    const minuteString =
+      minutes < 10 ? '0' + minutes.toFixed(0) : minutes.toFixed(0);
+    const remainingSeconds: number = seconds % 60;
+    const secondString =
+      remainingSeconds < 10 ? '0' + remainingSeconds : remainingSeconds;
+    return `${minuteString}m${secondString}s`;
+  }
+
+  /**
+   * This function creates an interaction based on the call being made.
+   *
+   * @memberof CallComponent
+   */
   ngOnInit() {
     const interactionDetails = new RecordItem('', '', '');
     interactionDetails.setPhone('phoneNumber', 'phoneNumber', this.number);
     this.interaction = {
-      interactionId: this.id,
+      interactionId: this.id.toString(),
       direction: InteractionDirectionTypes.Inbound,
       state: InteractionStates.Alerting,
       channelType: ChannelTypes.Telephony,
@@ -45,28 +79,84 @@ export class CallComponent implements OnInit {
     setInteraction(this.interaction);
   }
 
+  /**
+   * This function changes the state of the interaction to Connected
+   *
+   *
+   * @memberof CallComponent
+   */
   answerCall() {
     this.interaction.state = InteractionStates.Connected;
     setInteraction(this.interaction);
   }
 
+  /**
+   * This function ends the call when the "End Call" button is hit.
+   *
+   * @memberof CallComponent
+   */
   endCall() {
     this.interaction.state = InteractionStates.Disconnected;
     setInteraction(this.interaction);
     this.removeCall();
   }
 
+  /**
+   * This method drops the call when called by the endCall method call it.
+   *
+   * @memberof CallComponent
+   */
   removeCall() {
     this.callEnd.emit();
   }
+
+  /**
+   * This method puts the call on hold when the "Hold Call" button is hit
+   *
+   * @memberof CallComponent
+   */
   holdCall() {
+    console.log(`On hold at ${this.currentTime}`);
+    this.holdStart = this.currentTime;
     this.interaction.state = InteractionStates.OnHold;
+    const holdTimer = timer(0, 1000);
+    this.subscription = holdTimer.subscribe(
+      val => (this.currentHoldTime = val + this.totalHoldTime)
+    );
     setInteraction(this.interaction);
   }
+
+  /**
+   * This method takes the call off of hold when the "Unhold Call" nutton is hit.
+   *
+   * @memberof CallComponent
+   */
   unholdCall() {
+    console.log(`Off hold at ${this.currentTime}`);
+    this.holdEnd = this.currentTime;
+    this.holdTimes = [...this.holdTimes, this.holdEnd - this.holdStart];
+    console.log(`Hold Times: ${this.holdTimes}`);
+    this.subscription.unsubscribe();
+    this.totalHoldTime = this.calculateHold();
     this.interaction.state = InteractionStates.Connected;
     setInteraction(this.interaction);
   }
+
+  /**
+   * This method calculates the total time on hold from the hold times array.
+   *
+   * @returns the value hold
+   * @memberof CallComponent
+   */
+  calculateHold() {
+    let hold = 0;
+    for (let i = 0; i < this.holdTimes.length; i++) {
+      hold += this.holdTimes[i];
+    }
+    console.log(`Total Hold Time: ${hold}`);
+    return hold;
+  }
+
   getInteractionStateAsString(state: InteractionStates) {
     return InteractionStates[state];
   }
